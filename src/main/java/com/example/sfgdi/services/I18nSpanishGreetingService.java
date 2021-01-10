1c77500837283259335ccb0e98930d1673554e73
akshaypaswan <3kshay4aswan@gmail.com>
2021-01-11 00:37:25 +0530

    @Profile to switch between 2 language profiles

---
+++ src/main/java/com/example/sfgdi/services/I18nSpanishGreetingService.java
@@ -0,0 +1,13 @@
+package com.example.sfgdi.services;/* created by akshaypaswan on 11/01/21 */
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("ES")
+@Service("i18nService")
+public class I18nSpanishGreetingService implements GreetingService{
+    @Override
+    public String sayGreeting() {
+        return "Hello Mundo - ES";
+    }
+}
