be742760175d9e3c181663da06f9498916d7fd87
akshaypaswan <3kshay4aswan@gmail.com>
2021-01-17 16:53:11 +0530

    added spring environment to fetch properties from OS

--- src/main/java/com/example/sfgdi/config/PropertyConfig.java
+++ src/main/java/com/example/sfgdi/config/PropertyConfig.java
@@ -1,15 +1,19 @@
 package com.example.sfgdi.config;/* created by akshaypaswan on 17/01/21 */
 
 import com.example.sfgdi.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    private final Environment env;
 
     @Value("${data.user}")
     String user;
@@ -20,15 +24,21 @@ public class PropertyConfig {
     @Value("${data.url}")
     String url;
 
+    public PropertyConfig(Environment env) {
+        this.env = env;
+    }
+
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        //fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
     }
 
+    //required to load property source
     @Bean
     public static PropertySourcesPlaceholderConfigurer properties(){
         PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
